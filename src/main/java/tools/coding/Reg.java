23af1d283bd3b8d833aa743ca3e22d4b13aaea50
Theo Huang <Theo.Huang.C.H@outlook.com>
2014-09-26 13:48:34 +0800

    Reg

---
+++ src/main/java/tools/coding/Reg.java
@@ -0,0 +1,14 @@
+package tools.coding;
+
+import java.util.regex.Pattern;
+
+public class Reg {
+
+  public static final Pattern getIpReg() {
+    return Pattern.compile(
+        "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
+            + "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
+            + "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
+            + "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$");
+  }
+}
